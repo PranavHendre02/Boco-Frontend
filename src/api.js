e0d52368ec18168e556cc059918722548371f4f0
Pranav <pranavhendre07@gmail.com>
2025-07-10 00:54:53 +0530

    minor chnage

--- src/api.js
+++ src/api.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'https://boco-backend-q5zy.onrender.com/api',
-  
-  // Change to Render URL when deployed
 });
 
 export default API;
