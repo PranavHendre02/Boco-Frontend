ef4ef1637a99ef2885d414337d0741c365f004e5
Pranav <pranavhendre07@gmail.com>
2025-07-15 17:43:45 +0530

    added images via cloudiary

--- src/Components/CaseStudyCard.jsx
+++ src/Components/CaseStudyCard.jsx
@@ -1,32 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import API from '../api'; // baseURL: http://localhost:1337/api
-import {BACKEND_URL} from '../config'
 
 const CaseStudyCard = () => {
   const [cards, setCards] = useState([]);
 
   useEffect(() => {
     const fetchCards = async () => {
-      try {
-        const res = await API.get('/case-study-cards?populate=*');
-        console.log("✅ API Response:", res.data);
-        setCards(res.data.data || []);
-      } catch (error) {
-        console.error('Error fetching case study cards:', error);
-      }
+      const url1='https://res.cloudinary.com/drtosngvu/image/upload/v1752578590/sample9_920bc59582.png'
+      const url2='https://res.cloudinary.com/drtosngvu/image/upload/v1752578595/sample10_b594162259.png'
+      setCards([url1,url2])
     };
 
     fetchCards();
   }, []);
 
-  if (cards.length === 0) return <p className="text-center">s.</p>;
+  if (cards.length === 0) return <p className="text-center">.</p>;
 
   return (
     <div className="flex flex-wrap justify-center gap-8 px-8 py-10">
       {cards.map((card) => {
-        const imagePath = card.imageCaseCard?.formats?.small?.url || '';
-        const fullImageUrl = `${BACKEND_URL}${imagePath}`;
-
+        const fullImageUrl = `${card}`;
         return (
           <div key={card.id} className="rounded-xl overflow-hidden shadow-md">
             <img
